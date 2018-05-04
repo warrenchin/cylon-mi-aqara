a1a3b5b67a9e8ce861e0a4456717241fbbe24b07
warren <warren.chin@gmail.com>
2018-05-05 00:35:14 +0800

    Update driver name to avoid conflict, renamed 'status' to 'prop'

--- index.js
+++ index.js
@@ -3,35 +3,35 @@
 const Adaptor = require("./lib/adaptor");
 
 const Drivers = {
-	'gateway': require('./lib/gateway'),
-	'switch': require('./lib/switch'),
-	'sensor_ht': require('./lib/sensor_ht'),
-	'motion': require('./lib/motion'),
-	'magnet': require('./lib/magnet'),
-	'plug': require('./lib/plug'),
-	'wallswitch': require('./lib/wallswitch'),
-	'waterleak': require('./lib/waterleak')
+  'mi-aqara.gateway': require('./lib/gateway'),
+  'mi-aqara.switch': require('./lib/switch'),
+  'mi-aqara.sensor_ht': require('./lib/sensor_ht'),
+  'mi-aqara.motion': require('./lib/motion'),
+  'mi-aqara.magnet': require('./lib/magnet'),
+  'mi-aqara.plug': require('./lib/plug'),
+  'mi-aqara.wallswitch': require('./lib/wallswitch'),
+  'mi-aqara.waterleak': require('./lib/waterleak')
 };
 
 module.exports = {
-	// Adaptors your module provides, e.g. ["spark"]
-	adaptors: ["mi-aqara"],
-	drivers: Object.keys(Drivers),
+  // Adaptors your module provides, e.g. ["spark"]
+  adaptors: ["mi-aqara"],
+  drivers: Object.keys(Drivers),
 
-	// Modules intended to be used with yours, e.g. ["cylon-gpio"]
-	dependencies: [],
+  // Modules intended to be used with yours, e.g. ["cylon-gpio"]
+  dependencies: [],
 
-	adaptor: function (opts) {
-		return new Adaptor(opts);
-	},
+  adaptor: function (opts) {
+    return new Adaptor(opts);
+  },
 
-	driver: function (opts) {
-		opts = opts || {};
+  driver: function (opts) {
+    opts = opts || {};
 
-		if (Drivers[opts.driver]) {
-			return new Drivers[opts.driver](opts);
-		}
+    if (Drivers[opts.driver]) {
+      return new Drivers[opts.driver](opts);
+    }
 
-		return null;
-	}
+    return null;
+  }
 };
